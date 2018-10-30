cc5b2e1dea34526aa8cc0c1a1d8f018052f4557c
NaPuchta <43476735+NaPuchta@users.noreply.github.com>
2018-10-30 08:46:23 -0700

    added changes

--- Labs/Lab1026 ColorBars/colorbars.js
+++ Labs/Lab1026 ColorBars/colorbars.js
@@ -3,3 +3,30 @@
 **  Naomi Puchta
 **  Oct 23, 2018
 */
+
+// this creates the bars and puts them in the random order at the top
+function Bars(){
+
+}
+
+// this will be the bubble sorting method vv
+function bubbleSort(txt){
+	for(var i = txt.length - 1; i >= 1; i--){
+		for(var j = 0; j < txt.length; j++){
+			if(txt[j] > txt[j+1]){
+				console.log(txt);
+				n = n + 20
+				fill(255)
+				text(txt, 10, n)
+				tempor = txt[j+1];
+				txt[j+1] = txt[j];
+				txt[j] = tempor;
+			}
+		}
+	}
+
+
+// not quite sure how select sort will work so I'll have to go home and
+// figure it out vv
+// function selectSort(){
+// }
