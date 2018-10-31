97363c75310bf7a9cf5ab8d58a198b8f267cd945
NaPuchta <43476735+NaPuchta@users.noreply.github.com>
2018-10-30 23:33:56 -0700

    additions and reccomendations for code

--- Labs/Lab1026 ColorBars/colorbars.js
+++ Labs/Lab1026 ColorBars/colorbars.js
@@ -5,26 +5,39 @@
 */
 
 // this creates the bars and puts them in the random order at the top
-function Bars(){
-
+// really confused at this part, maybe move colors rather than moving
+// bars
+function Bars(loc,col){
+	 // Instance variables
+	 this.loc = loc;
+	 this.col = col;
 }
 
-// this will be the bubble sorting method vv
+this.render = function(){
+	fill(this.col);
+	// fill in with parameters... vv
+	rect();
+}
+
+// this will be the bubble sorting method vv (will sort by blue value)
 function bubbleSort(txt){
 	for(var i = txt.length - 1; i >= 1; i--){
 		for(var j = 0; j < txt.length; j++){
 			if(txt[j] > txt[j+1]){
 				console.log(txt);
+				// logs the process of switching
 				n = n + 20
 				fill(255)
 				text(txt, 10, n)
 				tempor = txt[j+1];
 				txt[j+1] = txt[j];
 				txt[j] = tempor;
+				// some of this code is obviously from the word sort so
+				// I need to find a way to change colors through this
+				// rather than text
 			}
 		}
 	}
-
 
 // not quite sure how select sort will work so I'll have to go home and
 // figure it out vv
--- Labs/Lab1026 ColorBars/sketch.js
+++ Labs/Lab1026 ColorBars/sketch.js
@@ -7,6 +7,8 @@
 // Global variables
 var bars = [];
 numb = 5
+xloc = 10
+yloc =
 
 //put setup code here
 function setup(){
@@ -15,14 +17,18 @@ function setup(){
 	background(205);
 	// calling function to load bars vv
 	loadBars(5)
+	// keeping these numbers small until it's figured out
 }
 
-// this loads in the bars
+// this loads in the bars vv
 function loadBars(amount){
 	for(var i = 0; i < amount; i++){
 		// this identifies what size and color the bar will be vv
-		var loc = createVector(random(width), random(height));
+		var loc = createVector(xloc,yloc);
 		var col = color(random(225),random(225),random(225));
-		bars.push(new Bars(loc, vel, radius, col));
+		bars.push(new Bars(loc,col));
+		// pushing bars into bar array
 	}
 }
+
+// Need to change a lot, figure out tomorrow..
