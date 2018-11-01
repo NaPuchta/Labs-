7dd6ceb660c33e67a79fd93cd460f946fe54f2dd
NaPuchta <43476735+NaPuchta@users.noreply.github.com>
2018-11-01 07:58:38 -0700

    changes

--- Labs/Lab1026 ColorBars/colorbars.js
+++ Labs/Lab1026 ColorBars/colorbars.js
@@ -4,27 +4,70 @@
 **  Oct 23, 2018
 */
 
-// this creates the bars and puts them in the random order at the top
-function Bars(){
+// restarting the code step by step
 
+function ColorBar(){
+	this.col;
+	this.loc;
 }
 
-// this will be the bubble sorting method vv
+this.run = function(){
+	this.render();
+}
+
+this.render = function(){
+	fill(this.col);
+	rect(this.loc.x,this.loc.y,100, 150);
+}
+
+
+
+
+
+
+
+
+
+
+
+// this creates the bars and puts them in the random order at the top
+<<<<<<< HEAD
+/* function Bars(){
+=======
+// really confused at this part, maybe move colors rather than moving
+// bars
+function Bars(loc,col){
+	 // Instance variables
+	 this.loc = loc;
+	 this.col = col;
+}
+>>>>>>> 97363c75310bf7a9cf5ab8d58a198b8f267cd945
+
+this.render = function(){
+	fill(this.col);
+	// fill in with parameters... vv
+	rect();
+}
+
+// this will be the bubble sorting method vv (will sort by blue value)
 function bubbleSort(txt){
 	for(var i = txt.length - 1; i >= 1; i--){
 		for(var j = 0; j < txt.length; j++){
 			if(txt[j] > txt[j+1]){
 				console.log(txt);
+				// logs the process of switching
 				n = n + 20
 				fill(255)
 				text(txt, 10, n)
 				tempor = txt[j+1];
 				txt[j+1] = txt[j];
 				txt[j] = tempor;
+				// some of this code is obviously from the word sort so
+				// I need to find a way to change colors through this
+				// rather than text
 			}
 		}
-	}
-
+*/	}
 
 // not quite sure how select sort will work so I'll have to go home and
 // figure it out vv
--- Labs/Lab1026 ColorBars/sketch.js
+++ Labs/Lab1026 ColorBars/sketch.js
@@ -7,6 +7,8 @@
 // Global variables
 var bars = [];
 numb = 5
+xloc = 10
+yloc =
 
 //put setup code here
 function setup(){
@@ -15,14 +17,62 @@ function setup(){
 	background(205);
 	// calling function to load bars vv
 	loadBars(5)
+	// keeping these numbers small until it's figured out
 }
 
+<<<<<<< HEAD
 // this loads in the bars
+
+function loadBars(numbars){
+	for(var i = 0; i < numbars; i++){
+		var loc = createVector(random(width),random(height));
+		var clr = color(random(255),random(255),random(255));
+		bars.push(new ColorBar(loc,clr));
+	}
+}
+
+function bubbleSort(){
+	var length = bars.length
+	for(var i = length - 1; i > 0; i--){
+		for(var j = 0; j < i; j++){
+			if(bars[j] > bars[j+1]){
+			var tmp = bars[j];
+			bars[j] = bars[j+1];
+			bars[j+1] = tmp;
+			}
+		}
+	}
+}
+
+function draw(){
+	background(20, 20, 20, 15);
+	for(var i = 0; i < bars.length; i++){
+		bars[i].run();
+	}
+}
+
+
+
+/* function loadBars(amount){
+=======
+// this loads in the bars vv
 function loadBars(amount){
+>>>>>>> 97363c75310bf7a9cf5ab8d58a198b8f267cd945
 	for(var i = 0; i < amount; i++){
 		// this identifies what size and color the bar will be vv
-		var loc = createVector(random(width), random(height));
+		var loc = createVector(xloc,yloc);
 		var col = color(random(225),random(225),random(225));
+<<<<<<< HEAD
+
 		bars.push(new Bars(loc, vel, radius, col));
 	}
 }
+*/
+=======
+		bars.push(new Bars(loc,col));
+		// pushing bars into bar array
+	}
+}
+
+// Need to change a lot, figure out tomorrow..
+>>>>>>> 97363c75310bf7a9cf5ab8d58a198b8f267cd945
