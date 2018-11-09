037ff98f8ced8e063276b102cd68a64226f6e057
NaPuchta <43476735+NaPuchta@users.noreply.github.com>
2018-11-09 08:44:57 -0800

    additions

--- Labs/Lab1101 GettingJSON/sketch.js
+++ Labs/Lab1101 GettingJSON/sketch.js
@@ -8,23 +8,34 @@ function setup(){
   cnv.position((windowWidth-width)/ 2, 30);
   background(5,5,5);
   fill(200,30,150);
+  // this is loading in the data from the population.json
   loadJSON("population.json", loadingData)
 }
 
 function loadingData(incomingData){
+  // giving the incomingData a variable "data"
   data = incomingData;
+  // this should be the bubble sorting function call vv
   bubbleSort(data)
 }
 
+// the draw function vv
+function draw(){
+
+}
+
+// bubble sorting function
 function bubbleSort(data){
 	for(var i = data.length - 1; i >= 1; i--){
 		for(var j = 0; j < data.length; j++){
-			if(data[j].females > data[j+1].females){
-				console.log(data);
-				tempor = data[j+1];
-				data[j+1] = data[j];
-				data[j] = tempor;
+      // checks to see if number of females is higher vv
+			if(data.countrydata[j].females > data.countrydata[j+1].females){
+				tempor = data.countrydata[j+1];
+				data.countrydata[j+1] = data.countrydata[j];
+				data.countrydata[j] = tempor;
 			}
 		}
 	}
+  // logs the end result of the data
+  console.log(data);
 }
