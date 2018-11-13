d6187c8413ec508c9faa372c5f6535f40fcfdca8
NaPuchta <43476735+NaPuchta@users.noreply.github.com>
2018-11-13 00:47:13 -0800

    changes to create bars
    still has some errors, not really sure on how to fix the sorting but worked on the bar creation so that when the sorting is figured out that the bars will be done sooner.

--- Labs/Lab1101 GettingJSON/sketch.js
+++ Labs/Lab1101 GettingJSON/sketch.js
@@ -4,6 +4,7 @@ var data;
 // there is no correlation between the two functions one is to collect
 // the data and the other is to make a canvas on the screen ???
 function setup(){
+  // creating the canvas vv
   var cnv = createCanvas (800,800);
   cnv.position((windowWidth-width)/ 2, 30);
   background(5,5,5);
@@ -19,20 +20,39 @@ function loadingData(incomingData){
   bubbleSort(data)
 }
 
-// the draw function vv
+// the draw function to make the bars vv
 function draw(){
-
+  starty = 5
+// need to add the same bars part so that each bar is drawn
+  for(var i = data.length - 1; i >=; i--){
+    for(var j = 0; j < data.length; j++){
+      // takes the total number of females
+      lenofbar = data[j].females
+      rect(2, starty, lenofbar, 3);
+      // creates a rect the length of the number of females
+      starty = starty + 3
+      // so that the next start will be y = 8 not y = 5 again
+      // note to self to add text somehow? not really sure how to fit
+      // text that is so small into the bars, perhaps try and instead
+      // fill in text above? or to the right of the bar
+      // text will say "Country: _____" and "Females: #"
+    }
+  }
 }
 
 // bubble sorting function
 function bubbleSort(data){
+  // starts at the end of the list vv
 	for(var i = data.length - 1; i >= 1; i--){
+    // moves to the beginning vv
 		for(var j = 0; j < data.length; j++){
       // checks to see if number of females is higher vv
-			if(data.countrydata[j].females > data.countrydata[j+1].females){
-				tempor = data.countrydata[j+1];
-				data.countrydata[j+1] = data.countrydata[j];
-				data.countrydata[j] = tempor;
+      // calls data, goes into country data [j] and goes into females
+			if(data[j].females > data[j+1].females){
+				tempor = data[j+1];
+				data[j+1] = data[j];
+				data[j] = tempor;
+        // this should go through the whole list
 			}
 		}
 	}
