99044b0ccbcaf479aef0280f3ffb66bcb4cf039d
NaPuchta <43476735+NaPuchta@users.noreply.github.com>
2018-11-15 08:42:48 -0800

    U left puter on

--- Labs/Lab1114 SnakeGame/sketch.js
+++ Labs/Lab1114 SnakeGame/sketch.js
@@ -3,22 +3,25 @@
 //  The setup function function is called once when your program begins
 
 var w;
-var col, row;
+var cols, rows;
 var snake;
 
 function setup() {
+  // this is the setup code
   var cnv = createCanvas(800, 800);
+  cols = width/w;
+  rows = height/w;
   cnv.position((windowWidth-width)/2, 30);
-  col = width/w;
-  row = height/w;
   background(5, 5, 5);
   fill(200, 30, 150);
-  snake = new Snake(createVector(width/2, height/2),createVector(1,0))
+  snake = new Snake(createVector(width/2, height/2),createVector(1,0));
+  snake.run();
+  console.log(snake)
 }
 
 //  The draw function is called @ 30 fps
 function draw() {
-  snake.run()
+  snake.run();
 }
 
 function keyPressed(){
--- Labs/Lab1114 SnakeGame/snake.js
+++ Labs/Lab1114 SnakeGame/snake.js
@@ -3,7 +3,6 @@ function Snake(loc,vel){
 
   this.loc = loc;
   this.vel = vel;
-
   this.run = function(){
     this.update();
     this.render();
@@ -12,12 +11,14 @@ function Snake(loc,vel){
   this.update = function(){
     this.vel.mult(w);
     this.loc.add(this.vel);
-    this.loc.x = constrain(this.loc.x,0, width-w);
-    this.loc.y - constrain(this.loc.y,0,height-w);
+    this.loc.x = constrain(this.loc.x, 0, width-w);
+    this.loc.y - constrain(this.loc.y, 0, height-w);
   }
 
   this.render = function(){
     fill(255,0,0);
-    rect(this.loc.x, this.loc.y, w,w);
+    rect(this.loc.x, this.loc.y, w, w);
   }
-}
+
+
+} // end of the snake function
