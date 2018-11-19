7b38ea8fd0c4b1f0e315661a1d530245ebb268d1
NaPuchta <43476735+NaPuchta@users.noreply.github.com>
2018-11-19 07:57:12 -0800

    some changes to the snake
    still having some issues with the cube moving, it's not moving smoothly across the screen and instead is going in "chunks" and moving past the screen when console.log is used.

--- Labs/Lab1114 SnakeGame/sketch.js
+++ Labs/Lab1114 SnakeGame/sketch.js
@@ -2,7 +2,7 @@
 //  This is a comment
 //  The setup function function is called once when your program begins
 
-var w;
+var w = 20;
 var cols, rows;
 var snake;
 
@@ -12,18 +12,17 @@ function setup() {
   cols = width/w;
   rows = height/w;
   cnv.position((windowWidth-width)/2, 30);
-  background(5, 5, 5);
-  fill(200, 30, 150);
+  frameRate(1)
+  background(20, 20, 20);
   snake = new Snake(createVector(width/2, height/2), createVector(1,0));
-  snake.run();
-  console.log(snake)
 }
 
-//  The draw function is called @ 30 fps
+//  draw function creates the snake
 function draw() {
   snake.run();
 }
 
+// checks to see if the keys are pressed to make the cube move
 function keyPressed(){
   if(keyCode === UP_ARROW){
     snake.vel - createVector(0,-1*w);
--- Labs/Lab1114 SnakeGame/snake.js
+++ Labs/Lab1114 SnakeGame/snake.js
@@ -3,6 +3,7 @@ function Snake(loc,vel){
 
   this.loc = loc;
   this.vel = vel;
+
   this.run = function(){
     this.update();
     this.render();
@@ -11,8 +12,9 @@ function Snake(loc,vel){
   this.update = function(){
     this.vel.mult(w);
     this.loc.add(this.vel);
-    this.loc.x = constrain(this.loc.x, 0, width-w);
-    this.loc.y - constrain(this.loc.y, 0, height-w);
+    // keeps the cube within a certain amount
+    this.loc.x = constrain(this.loc.x, 0, width - w);
+    this.loc.y - constrain(this.loc.y, 0, height - w);
   }
 
   this.render = function(){
